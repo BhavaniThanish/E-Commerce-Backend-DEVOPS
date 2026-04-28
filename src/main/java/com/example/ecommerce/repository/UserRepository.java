2addb6fefee71adad67a8400fbc8612fd990b27a
Shubham Kumar <256505386+Shubham1740b-cmyk@users.noreply.github.com>
2026-04-28 22:48:14 +0530

    MongoDB repository for Persistence
    This is an Interface that extends MongoRepository. It acts as the bridge between your Java code and the database. It provides built-in methods like save() and findByUsername(), so you don't have to write complex SQL or NoSQL queries manually.

--- src/main/java/com/example/ecommerce/repository/UserRepository.java
+++ src/main/java/com/example/ecommerce/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.ecommerce.repository;
 
-
 import com.example.ecommerce.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository; 
