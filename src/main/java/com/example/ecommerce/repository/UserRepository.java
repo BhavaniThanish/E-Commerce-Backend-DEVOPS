4426f4a85627f7e6d02481de25d633c5542dd401
Shubham Kumar <256505386+Shubham1740b-cmyk@users.noreply.github.com>
2026-04-28 22:58:01 +0530

    MongoDB repository
    This is an Interface that extends MongoRepository. It acts as the bridge between our Java code and the database. It provides built-in methods like save() and findByUsername(), so we don't have to write complex SQL or NoSQL queries manually. Just Used these Methods In My Code.

--- src/main/java/com/example/ecommerce/repository/UserRepository.java
+++ src/main/java/com/example/ecommerce/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.example.ecommerce.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository; 
 
-
 import java.util.Optional;
 
 @Repository
